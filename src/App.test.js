d031b98d5bab8abd062087715a1c510ab8615474
Henna <henna.tuominen@tuni.fi>
2022-01-02 22:16:27 +0200

    Removed unused files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Hackacoin app title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hackacoin/i);
-  expect(linkElement).toHaveTextContent();
-});
